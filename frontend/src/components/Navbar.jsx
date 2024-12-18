e39be92ac80718d566a47b348bf02767762768c2
08Abhijeet <abhijeetkadam656@gmail.com>
2024-12-18 20:46:41 +0530

    updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useAuthStore } from "../store/useAuthStore";
-import { LogOut, MessageSquare, Settings, User } from "lucide-react";
-
+import { LogOut, Settings, User } from "lucide-react";
+import { PiChatsCircleDuotone } from "react-icons/pi";
 const Navbar = () => {
   const { logout, authUser } = useAuthStore();
 
@@ -13,11 +13,19 @@ const Navbar = () => {
       <div className="container mx-auto px-4 h-16">
         <div className="flex items-center justify-between h-full">
           <div className="flex items-center gap-8">
-            <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
+            <Link
+              to="/"
+              className="flex items-center gap-2.5 hover:opacity-80 transition-all"
+            >
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
-                <MessageSquare className="w-5 h-5 text-primary" />
+                <PiChatsCircleDuotone
+                  className="inline-block"
+                  size={22}
+                  color="gold"
+                />
               </div>
-              <h1 className="text-lg font-bold">Chatty</h1>
+              <h1 className="text-lg font-bold">Chatify</h1>{" "}
+              {/* Adjust text size here */}
             </Link>
           </div>
 
