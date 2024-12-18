8e4dcdc6883c98120a8d4a0d377059abcda196ea
08Abhijeet <abhijeetkadam656@gmail.com>
2024-12-18 22:21:56 +0530

    gateway crash

--- backend/src/index.js
+++ backend/src/index.js
@@ -43,3 +43,6 @@ server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
 });
+
+server.keepAliveTimeout = 120000; // 120 seconds
+server.headersTimeout = 120000; 
